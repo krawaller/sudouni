212901b5e984e757acb549a1ac5d9366371c8cfe
KungD <david@krawaller.se>
2014-11-05 12:38:48 +0100

    fix

--- src/components/sudosel.jsx
+++ src/components/sudosel.jsx
@@ -3,10 +3,13 @@
 var Sudoselect = React.createClass({
   render: function(){
     return (
-      <div className='btn-group'>
-        {_.map(sudo.sudos,function(def,key){          
-          return <button key={key} className='btn btn-default' onClick={_.partial(actions.selectSudo,key)}>{key}</button>;
-        },this) }
+      <div>
+        <p>Select a sudoku</p>
+        <div className='btn-group'>
+          {_.map(sudo.sudos,function(def,key){          
+            return <button key={key} className='btn btn-default' onClick={_.partial(actions.selectSudo,key)}>{key}</button>;
+          },this) }
+        </div>
       </div>
     );
   }
